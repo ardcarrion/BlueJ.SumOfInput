d964dc1d6416fa375b6a464308651c95e572993f
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-22 20:36:02 -0400

    for loop sum 1 to n

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Input an integer: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
 
+       for (int i = 1; i <= n; i++) {
+              sum += i;
+       }
+       System.out.format("The sum of 1 to %d is: %d \n", n, sum); 
     }
 }
